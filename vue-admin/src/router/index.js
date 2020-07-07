8686063f737e00f0267b8bd9e3920e939409d27f
柯Father <813871604@qq.com>
2020-07-07 14:52:26 +0800

    构建项目，配置全局样式，路由，element-ui

---
+++ vue-admin/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "Login"
+  },
+  {
+    path: "/Login",
+    name: "Login",
+    component: () => import("@/views/Login/Login.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
